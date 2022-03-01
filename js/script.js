719a7802bc9b3f496a23b1c9a34b4ae7e0c39dfa
AriyanShihab <devshihab111@gmail.com>
2022-03-01 20:59:06 +0600

    add diffaren colurs

--- js/script.js
+++ js/script.js
@@ -3,27 +3,33 @@ $(".slider").owlCarousel({
     autoplay: true,
     autoplayTimeout: 2000, //2000ms = 2s;
     autoplayHoverPause: true,
-    nav: false,
+    nav: true,
     responsive: {
         0: {
             items: 1,
+            nav: true,
         },
         600: {
             items: 2,
+            nav: false,
         },
         1000: {
             items: 3,
+            nav: true,
+        },
+        1200: {
+            items: 4,
+            nav: true,
         },
     },
 });
 
-// dark lightt
+// dark light
 
 let ligh = document.getElementById(`light`);
 let dark = document.getElementById(`dark`);
-console.log(dark);
 
-light.addEventListener(`click`, () => {
+ligh.addEventListener(`click`, () => {
     document.documentElement.removeAttribute("data-theme", "light");
 });
 
